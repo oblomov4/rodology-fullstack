f80f19173224f21956a0d4cbffc8556825f0ba09
oblomov4 <vlad.oblomov4@gmail.com>
2025-06-01 12:42:04 +0300

    mask

--- public/js/main.js
+++ public/js/main.js
@@ -36,3 +36,18 @@ function showToast(type, message) {
   toast.className = 'toast-visible';
   setTimeout(() => (toast.className = 'toast-hidden'), 3000);
 }
+
+document.querySelectorAll('.phone-input').forEach((item) =>
+  item.addEventListener('input', function (e) {
+    const x = e.target.value
+      .replace(/\D/g, '')
+      .match(/(\d{0,1})(\d{0,3})(\d{0,3})(\d{0,2})(\d{0,2})/);
+    e.target.value = !x[2]
+      ? '+7'
+      : '+7 (' +
+        x[2] +
+        (x[3] ? ') ' + x[3] : '') +
+        (x[4] ? '-' + x[4] : '') +
+        (x[5] ? '-' + x[5] : '');
+  }),
+);
